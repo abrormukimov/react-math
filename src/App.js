6536aaaa6622aab70dffb2b033613e6acced5456
abrormukimov <abrormm34@gmail.com>
2022-01-15 10:12:34 +0500

    Refactor using hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import { React, Component } from 'react';
 import Calculator from './components/Calculator';
-// eslint-disable-next-line react/prefer-stateless-function
-class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <Calculator />
-      </div>
-    );
-  }
+
+function App() {
+  return (
+    <div className="container">
+      <Calculator />
+    </div>
+  );
 }
+
 export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CalculatorOperations from './Buttons/CalculatorOperations';
 import Numbers from './Buttons/Numbers';
 import SimpleOperations from './Buttons/SimpleOperations';
@@ -6,31 +6,24 @@ import classes from './Calculator.module.css';
 import Input from './Buttons/Input';
 import calculate from './logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { calc: {}, innerText: '' };
-    this.changeHandler = this.changeHandler.bind(this);
-  }
-
-  changeHandler(e) {
-    const { calc } = this.state;
+function Calculator() {
+  const [currentInnerText, setInnerText] = useState('');
+  const [calc, setCalc] = useState({});
+  const changeHandler = (e) => {
     const buttonName = e.target.name;
     const newCalc = calculate(calc, buttonName);
-    this.setState({ calc: newCalc, innerText: `${newCalc.next ? newCalc.next : ''} ${newCalc.operation ? newCalc.operation : ''} ${newCalc.total ? newCalc.total : ''}` });
-  }
+    setCalc(newCalc);
+    setInnerText(`${newCalc.next ? newCalc.next : ''} ${newCalc.operation ? newCalc.operation : ''} ${newCalc.total ? newCalc.total : ''}`);
+  };
 
-  render() {
-    const { innerText } = this.state;
-    return (
-      <div className={classes.container}>
-        <Input value={innerText} />
-        <CalculatorOperations onclick={this.changeHandler} />
-        <Numbers onclick={this.changeHandler} />
-        <SimpleOperations onclick={this.changeHandler} />
-      </div>
-    );
-  }
+  return (
+    <div className={classes.container}>
+      <Input value={currentInnerText} />
+      <CalculatorOperations onclick={changeHandler} />
+      <Numbers onclick={changeHandler} />
+      <SimpleOperations onclick={changeHandler} />
+    </div>
+  );
 }
 
 export default Calculator;
